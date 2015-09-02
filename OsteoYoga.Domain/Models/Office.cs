2cd6002ccbfdf40745be520ad09d91d3d8a81be4
Quentin Truffart <quentin.truffart@gmail.com>
2015-09-02 14:14:20 +0200

    Ajout de la classe Office

---
+++ OsteoYoga.Domain/Models/Office.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OsteoYoga.Domain.Models
+{
+    public class Office : Entity
+    {
+        [Required]
+        public virtual string Name { get; set; }
+    }
+}
