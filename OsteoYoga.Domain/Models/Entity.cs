6f38f35b4a978e92005568619109aaa21f485d9a
Quentin Truffart <quentin.truffart@gmail.com>
2015-09-02 17:13:47 +0200

    Stabilisation avec référence nuget

--- OsteoYoga.Domain/Models/Entity.cs
+++ OsteoYoga.Domain/Models/Entity.cs
@@ -2,41 +2,6 @@
 {
     public class Entity : IEntity
     {
-       
-
         public virtual int Id { get; set; }
-
-
-        // Let is now override the equality operator
-        // == and != comes in pair, if we define one we need to define other too.
-        public static bool operator ==(Entity rational1, Entity rational2)
-        {
-            return rational2 != null && (rational1 != null && rational1.Id == rational2.Id);
-        }
-
-        public static bool operator !=(Entity rational1, Entity rational2)
-        {
-            return rational2 != null && (rational1 != null && rational1.Id != rational2.Id);
-        }
-
-        public override bool Equals(object obj)
-        {
-            Entity r = obj as Entity;
-            if (r != null)
-            {
-                return r == this;
-            }
-            return false;
-        }
-
-        protected bool Equals(Entity other)
-        {
-            return Id == other.Id;
-        }
-
-        public override int GetHashCode()
-        {
-            return Id;
-        }
     }
 }
